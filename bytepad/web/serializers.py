62907f2cf259feb000f0ead366a00cfd77acdcef
Bansal Utkarsh <bansalutkarsh3@gmail.com>
2017-03-20 00:13:34 +0530

    Modify List view to show string instead of pk for session & exam

--- bytepad/web/serializers.py
+++ bytepad/web/serializers.py
@@ -1,8 +1,17 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework import serializers
 from models import Paper
 
 
-class PaperSerilaizer(ModelSerializer):
+class PaperSerilaizer(serializers.ModelSerializer):
+    exam = serializers.CharField()
+    session = serializers.CharField()
+
+    def get_exam(self):
+        return self.exam.name
+
+    def get_session(self):
+        return self.session.year
+
     class Meta:
         model = Paper
         fields = ('name', 'file', 'exam', 'session')
