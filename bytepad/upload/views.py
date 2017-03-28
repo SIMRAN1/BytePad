ff5389814002f6bb1c94c111655ac08e6e898658
Bansal Utkarsh <bansalutkarsh3@gmail.com>
2017-03-29 02:09:36 +0530

    Authentication fixes
    * Logged in user cant access login page
    * AnonymousUser cant access upload page

--- bytepad/upload/views.py
+++ bytepad/upload/views.py
@@ -1,16 +1,22 @@
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import HttpResponseRedirect
 from django.views.generic import FormView
+from web.models import Paper
 
 from forms import UploadForm
-from web.models import Exam, Session, Paper
 
 
 class LoginView(FormView):
     template_name = 'login.html'
     success_url = '/admin/'
     form_class = AuthenticationForm
+
+    def get(self, request):
+        if self.request.user.is_authenticated():
+            return HttpResponseRedirect(self.success_url)
+        return super(LoginView, self).get(request)
 
     def form_valid(self, form):
         username = form.cleaned_data['username']
@@ -24,7 +30,9 @@ class LoginView(FormView):
             return self.form_invalid(form)
 
 
-class UploadView(FormView):
+class UploadView(LoginRequiredMixin, FormView):
+    login_url = '/admin/login/'
+    redirect_field_name = 'next'
     template_name = 'upload.html'
     success_url = '/'
     form_class = UploadForm
@@ -48,5 +56,3 @@ class UploadView(FormView):
             papers.append(Paper(name=f.name, file=f, exam=exam, session=session))
         Paper.objects.bulk_create(papers)
         return HttpResponseRedirect(self.get_success_url())
-
-
