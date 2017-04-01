be37e77d46381ec1584e43cdf2c425898b729b15
Bansal Utkarsh <bansalutkarsh3@gmail.com>
2017-04-02 00:28:26 +0530

    Adds basic css

--- bytepad/web/urls.py
+++ bytepad/web/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import url
+
 from views import PaperListAPIView, LastUpdateAPIView, HomeView, SearchView
 
 urlpatterns = [
     url(r'^api/list/$', PaperListAPIView.as_view(), name='list-papers'),
     url(r'^api/last-update/$', LastUpdateAPIView.as_view(), name='last-update'),
-    url(r'^home/$', HomeView.as_view(), name='home'),
+    url(r'^$', HomeView.as_view(), name='home'),
     url(r'^search/(?P<query>[\w\-]+)/$', SearchView.as_view(), name='search'),
 ]
